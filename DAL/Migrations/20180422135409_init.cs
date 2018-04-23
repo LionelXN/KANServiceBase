1a67f2f7536c5e7598839c8a3a9b6ab454308937
超级逗逗飞 <nn260371345@hotmail.com>
2018-04-23 22:22:05 +0800

    KAN001
    KAN001

---
+++ DAL/Migrations/20180422135409_init.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace DAL.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(maxLength: 500, nullable: false),
+                    Discount = table.Column<decimal>(nullable: false),
+                    Name = table.Column<string>(maxLength: 20, nullable: false),
+                    Price = table.Column<decimal>(nullable: false),
+                    RowVersion = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Courses");
+        }
+    }
+}
---
+++ Entity/Table/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entity.Core;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entity.Table
+{
+    /// <summary>
+    /// 商品类
+    /// </summary>
+    public class Product : BaseEntity<long>
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        [StringLength(20)]
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        [StringLength(500)]
+        [Required]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 原价
+        /// </summary>
+        [Required]
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// 现价
+        /// </summary>
+        public decimal Discount { get; set; }
+    }
+}
